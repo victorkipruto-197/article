6d72ee55963bb6489d939021c3ad09204a48ce2e
Victor Kipruto Kosgei <VKosgei@LTTECHMAC1060.local>
2025-07-20 19:04:02 +0300

    build release 1.0.5

--- dist/repositories.d.ts
+++ dist/repositories.d.ts
@@ -1,5 +1,6 @@
 import { CreateUserForm, Log, Role, User } from "./entities";
 export interface SiteDB {
+    populateUserRoles(): Promise<boolean>;
     createUser(request: CreateUserForm): Promise<User | null>;
     assignRoleToUser(userId: string, role: Role): Promise<boolean>;
     getUserById(userId: string): Promise<User | undefined>;
