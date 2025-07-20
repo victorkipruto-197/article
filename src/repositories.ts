5af9e66e664bde2707edd08479b5f74cc9834f4e
Victor Kipruto Kosgei <VKosgei@LTTECHMAC1060.local>
2025-07-20 19:03:10 +0300

    added use api populateUserRoles

--- src/repositories.ts
+++ src/repositories.ts
@@ -5,6 +5,7 @@ import { CError, CreateUserForm, Log, Role, User } from "./entities";
 // }
 
 export interface SiteDB{
+    populateUserRoles():Promise<boolean>
     createUser(request:CreateUserForm):Promise<User|null>
     assignRoleToUser(userId:string, role:Role):Promise<boolean>
     getUserById(userId:string):Promise<User|undefined>
